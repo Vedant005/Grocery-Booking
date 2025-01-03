72bc76aad148b235763b104eaf5c8df65f27d244
Vedant005 <vedantkanekar909@gmail.com>
2025-01-03 14:41:13 +0530

    feat: refactor app.js to consolidate routing under a single routes module

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 import express from "express";
-
+import routes from "./routes";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
-import userRouter from "./routes/user.routes.js";
-import productRouter from "./routes/product.routes.js";
-import orderRouter from "./routes/order.routes.js";
-app.use("/api/users", userRouter);
-app.use("/api/product", productRouter);
-app.use("/api/orders", orderRouter);
+app.use("/api", routes);
 
 export { app };
