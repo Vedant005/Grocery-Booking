346d871fff0e5599078863b94d849e04a0b69a74
Vedant005 <vedantkanekar909@gmail.com>
2025-01-03 14:36:36 +0530

    refactor: renamed controllers, models, and routes for orders, products, and users

--- src/routes/order.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { placeOrder, viewOrders } from "../controllers/order.controller.js";
-
-const router = express.Router();
-
-router.post("/orders", placeOrder);
-router.get("/orders/:userId", viewOrders);
-
-export default router;
--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUsers, login, register } from "../controllers/user.controller.js";
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-router.get("/getAll", getUsers);
-
-export default router;
